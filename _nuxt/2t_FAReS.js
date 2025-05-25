147cf5612b350e807eaf737a392787bb80f89a67
Michael Schmid <michael@on-running.com>
2025-05-25 20:46:26 +0200

    Deploy: 2025-05-25 20:46:26 (build ID: 55cf69cc-c858-4f92-95c1-bd70e351e8d9)

---
+++ _nuxt/2t_FAReS.js
@@ -0,0 +1,39 @@
+import{u as s}from"./BDK0L30x.js";const g=`query GetPage($slug: String!) {
+    pages(filters: { slug: { eqi: $slug } }, pagination: { limit: 1 }, status: DRAFT) {
+        documentId
+        title
+        slug
+        seo {
+            metaTitle
+            metaDescription
+        }
+        blocks {
+          highlight {
+            name
+            slug
+            title
+            description
+            headerImage {
+              caption
+              url
+              alternativeText
+            }
+            categories_connection {
+              nodes {
+                slug
+                name
+                description
+              }
+            }
+            categories {
+              name
+              slug
+              description
+            }
+            createdAt
+            updatedAt
+            publishedAt
+          }
+        }
+    }
+} `,r=()=>{const{query:i}=s();return{getPageBySlug:async t=>{var e;const n=await i(g,{slug:t});return(e=n==null?void 0:n.pages)==null?void 0:e[0]},getHighlightBlocks:t=>t.blocks.map(n=>n.highlight).filter(n=>n!==void 0)}};export{r as u};
