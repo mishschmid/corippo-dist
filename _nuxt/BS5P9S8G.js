d92f0c8099b379c2a782435ceeb21223b3ac9c87
Michael Schmid <michael@on-running.com>
2025-05-26 09:30:04 +0200

    Deploy: 2025-05-26 09:30:04 (build ID: 8555dace-a1bf-4edd-9cb9-e08c9549e0e6)

--- _nuxt/BS5P9S8G.js
+++ _nuxt/BS5P9S8G.js
@@ -1,4 +1,4 @@
-import{u as s}from"./C0qozeAI.js";const g=`query GetPage($slug: String!) {
+import{u as s}from"./D5IPZqFS.js";const g=`query GetPage($slug: String!) {
     pages(filters: { slug: { eqi: $slug } }, pagination: { limit: 1 }, status: DRAFT) {
         documentId
         title
